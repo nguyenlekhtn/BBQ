9cce7c1b76d62a70a0dd08349cf36ad562b1e6d9
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-06-29 11:11:42 +0700

    Finish project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+module.exports = config;
